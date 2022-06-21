b3e0a0d6c425b2555a82a92533d56aa130bb2d29
Rebecca Peckman <rebeccapeckman@gmail.com>
2022-06-21 15:58:34 -0700

    simple hello world function

---
+++ HelloWorld.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity >=0.7.0 <0.9.0;
+
+contract HelloWorld {
+    function hello() public pure returns (string memory) {
+        return "Hello world";
+    }
+}
