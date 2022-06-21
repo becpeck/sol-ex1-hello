d94adc13daf972ad156786e77f50dbe80a26d232
Rebecca Peckman <rebeccapeckman@gmail.com>
2022-06-21 16:22:51 -0700

    message stored and updatable by function

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -2,17 +2,26 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract HelloWorld {
+
+    string message = "Hello world";
     event Message(string message);
 
     // function hello() public pure returns (string memory) {
     //     return "Hello world";
     // }
 
-    // Emitting an event makes function neither view nor pure
+    // // Emitting an event makes function neither view nor pure
+    // function hello() public returns (string memory) {
+    //     emit Message("Hello world");
+    //     return "Hello world";
+    // }
+
     function hello() public returns (string memory) {
-        emit Message("Hello world");
-        return "Hello world";
+        emit Message(message);
+        return message;
     }
 
-
+    function updateMessage(string memory newMessage) public {
+        message = newMessage;
+    }
 }
