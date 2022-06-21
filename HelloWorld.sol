a20917f7d4f82718eaa3978ff5cf4e9bfe86a7f9
Rebecca Peckman <rebeccapeckman@gmail.com>
2022-06-21 16:10:51 -0700

    added message event

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -2,7 +2,17 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract HelloWorld {
-    function hello() public pure returns (string memory) {
+    event Message(string message);
+
+    // function hello() public pure returns (string memory) {
+    //     return "Hello world";
+    // }
+
+    // Emitting an event makes function neither view nor pure
+    function hello() public returns (string memory) {
+        emit Message("Hello world");
         return "Hello world";
     }
+
+
 }
